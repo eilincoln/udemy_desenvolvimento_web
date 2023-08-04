bc648f94414a07c526704cdd1d58784fc2cf2a06
eilincoln <115198997+eilincoln@users.noreply.github.com>
2023-08-04 09:52:34 -0300

    envio do segundo projeto
    projeto ensinando sobre variáveis e como usar elas

---
+++ projeto_02/main.js
@@ -0,0 +1,10 @@
+let firstName = 'Lincoln'
+let lastName = 'Berto'
+let age = 25
+
+
+console.log(firstName)
+console.log(age)
+
+console.log(`my name is ${firstName} ${lastName}`)
+console.log(`I am ${age} years old`)
