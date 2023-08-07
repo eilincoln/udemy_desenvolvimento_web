a199a7029746d0a09bd63596843bcbfded56d2d4
eilincoln <115198997+eilincoln@users.noreply.github.com>
2023-08-07 10:41:18 -0300

    add_projeto06

---
+++ projeto_06/main.js
@@ -0,0 +1,11 @@
+let amigos = ['alan', 'mark', 'julia', 'bella', 1]
+console.log(amigos)
+console.log(amigos[1])
+
+
+
+amigos[1] = 'maverick'
+console.log(amigos[1])
+
+amigos[5] = 'lincoln'
+console.log(amigos[5])
