3fa50f5059f267693ccd0766655f3eccb6a77ae0
eilincoln <115198997+eilincoln@users.noreply.github.com>
2023-08-07 09:47:57 -0300

    add_projeto03
    Adicionando o projeto 03 do curso. É basicamente ensinando a receber inputs do usuário.

---
+++ projeto_03/main.js
@@ -0,0 +1,7 @@
+let firstName = prompt('type your first name:');
+let lastName = prompt('type your last name:');
+let age = parseInt(prompt('type your age:')) 
+const age2 = 2
+
+
+console.log(`your name is: ${firstName} ${lastName} and you are ${age + age2} years old! `)
