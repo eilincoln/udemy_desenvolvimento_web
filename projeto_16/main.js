46ecde69995a99e20a24cf6e08c967386ac44681
eilincoln <115198997+eilincoln@users.noreply.github.com>
2023-08-14 10:17:28 -0300

    Add files via upload

---
+++ projeto_16/main.js
@@ -0,0 +1,4 @@
+function trocarCor() {
+    let titleH1 = document.getElementById('h1') // primeiro eu pego o item e transformo em uma variavel 
+    titleH1.style.color = 'red' // aqui eu mudo a cor do texto 
+}
